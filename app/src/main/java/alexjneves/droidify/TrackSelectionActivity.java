b1f50242f3614b75b3103b82dbc8fef312b1b5f6
Alex Neves <alexneves08@gmail.com>
2015-11-02 16:56:38 +0000

    Initial commit

---
+++ app/src/main/java/alexjneves/droidify/TrackSelectionActivity.java
@@ -0,0 +1,13 @@
+package alexjneves.droidify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrackSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_selection);
+    }
+}
